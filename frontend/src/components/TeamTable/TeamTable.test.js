7185c45ee9331d6e4635f02498f71223470cc372
PotatoOverlord22 <raul.tiron@stud.ubbcluj.ro>
2024-03-20 23:25:09 +0200

    add proper delete tests

--- frontend/src/components/TeamTable/TeamTable.test.js
+++ frontend/src/components/TeamTable/TeamTable.test.js
@@ -6,41 +6,41 @@ import user from '@testing-library/user-event'
 
 // mock
 const teamsList = [
-    { id: 1, name: 't1', region: 'r1', players: [{name: "test-name", position: "test-position", kda: 0.01}, {name: "test-name2", position: "test-position2", kda: 0.02}]},
-    { id: 2, name: 't2', region: 'r2', players: [] }
+  { id: 1, name: 't1', region: 'r1', players: [{ name: "test-name", position: "test-position", kda: 0.01 }, { name: "test-name2", position: "test-position2", kda: 0.02 }] },
+  { id: 2, name: 't2', region: 'r2', players: [] }
 ];
 
 test('test correct headings and buttons are displayed', () => {
-    const { getByText } = render(<TeamTable teamsList={teamsList} />);
-    expect(getByText('Esports teams')).toBeInTheDocument()
-    expect(getByText('Logo')).toBeInTheDocument()
-    expect(getByText('Team Name')).toBeInTheDocument()
-    expect(getByText('Region')).toBeInTheDocument()
-    expect(getByText('Actions')).toBeInTheDocument()
-    expect(getByText('Add new team')).toBeInTheDocument()
+  const { getByText } = render(<TeamTable teamsList={teamsList} />);
+  expect(getByText('Esports teams')).toBeInTheDocument()
+  expect(getByText('Logo')).toBeInTheDocument()
+  expect(getByText('Team Name')).toBeInTheDocument()
+  expect(getByText('Region')).toBeInTheDocument()
+  expect(getByText('Actions')).toBeInTheDocument()
+  expect(getByText('Add new team')).toBeInTheDocument()
 });
 
 test('test teams to be displayed', () => {
-    const { getByText } = render(<TeamTable teamsList={teamsList} />);
-    expect(getByText('t1')).toBeInTheDocument()
-    expect(getByText('t2')).toBeInTheDocument()
+  const { getByText } = render(<TeamTable teamsList={teamsList} />);
+  expect(getByText('t1')).toBeInTheDocument()
+  expect(getByText('t2')).toBeInTheDocument()
 });
 
 test('test regions to be displayed', () => {
-    const { getByText } = render(<TeamTable teamsList={teamsList}/>)
-    expect(getByText('r1')).toBeInTheDocument()
-    expect(getByText('r2')).toBeInTheDocument()
+  const { getByText } = render(<TeamTable teamsList={teamsList} />)
+  expect(getByText('r1')).toBeInTheDocument()
+  expect(getByText('r2')).toBeInTheDocument()
 })
 
 test('test add new team form subcomponent', () => {
-    const { getByText } = render(<TeamTable teamsList={teamsList} />);
-    // get the add team button
-    const addButton = getByText('Add new team');
-    // click on the button
-    fireEvent.click(addButton);
-    expect(getByText('Add a new team')).toBeInTheDocument();
-    expect(getByText('Save new team')).toBeInTheDocument();
-    expect(getByText('Cancel')).toBeInTheDocument();
+  const { getByText } = render(<TeamTable teamsList={teamsList} />);
+  // get the add team button
+  const addButton = getByText('Add new team');
+  // click on the button
+  fireEvent.click(addButton);
+  expect(getByText('Add a new team')).toBeInTheDocument();
+  expect(getByText('Save new team')).toBeInTheDocument();
+  expect(getByText('Cancel')).toBeInTheDocument();
 });
 
 test('test adding a team to the component', () => {
@@ -71,7 +71,7 @@ test('should trigger edit team when edit button is clicked', () => {
 });
 
 test("test editing a team's fields", () => {
-  const { getAllByText, getByText, getByTestId, getAllByTestId, container} = render(<TeamTable teamsList={teamsList} />)
+  const { getAllByText, getByText, getByTestId, getAllByTestId, container } = render(<TeamTable teamsList={teamsList} />)
   // click on the first edit button
   fireEvent.click(getAllByText('Edit')[0]);
 
@@ -89,12 +89,12 @@ test("test editing a team's fields", () => {
 
   fireEvent.change(editNameInput, { target: { value: 'edited team name' } });
   fireEvent.change(editRegionInput, { target: { value: 'edited region name' } });
-  
+
   // only gonna test the first player to edit
-  fireEvent.change(editPlayerPositionInputs[0], { target: { value: 'edited player position'} });
+  fireEvent.change(editPlayerPositionInputs[0], { target: { value: 'edited player position' } });
   console.log(editPlayerPositionInputs[0])
-  fireEvent.change(editPlayerKdaInputs[0], { target: { value: 3.14} });
-  fireEvent.change(editPlayerNameInputs[0], { target: { value: 'edited player name'} });
+  fireEvent.change(editPlayerKdaInputs[0], { target: { value: 3.14 } });
+  fireEvent.change(editPlayerNameInputs[0], { target: { value: 'edited player name' } });
 
   // click save
   fireEvent.click(saveButton);
@@ -112,19 +112,28 @@ test("test editing a team's fields", () => {
 });
 
 test('test search function based on region', () => {
-    const teamsList = [
-      { id: 1, name: 't1', region: 'europe', players: [] },
-      { id: 2, name: 't2', region: 'NORTH AMERICA', players: [] },
-      { id: 3, name: 't3', region: 'EUROPEEE', players: [] },
-    ];
-    const { getByPlaceholderText, getByText } = render(<TeamTable teamsList={teamsList} />);
-    
-    const searchInput = getByPlaceholderText('Search by region...');
+  const teamsList = [
+    { id: 1, name: 't1', region: 'europe', players: [] },
+    { id: 2, name: 't2', region: 'NORTH AMERICA', players: [] },
+    { id: 3, name: 't3', region: 'EUROPEEE', players: [] },
+  ];
+  const { getByPlaceholderText, getByText } = render(<TeamTable teamsList={teamsList} />);
 
-    fireEvent.change(searchInput, { target: { value: 'europe' } });
-    // t1 and t3 should be on the screen  
-    expect(getByText('t1')).toBeInTheDocument();
-    expect(getByText('t3')).toBeInTheDocument();
-    // t2 should not be on the screen
-    expect(() => getByText('t2')).toThrow();
-  });
+  const searchInput = getByPlaceholderText('Search by region...');
+
+  fireEvent.change(searchInput, { target: { value: 'europe' } });
+  // t1 and t3 should be on the screen  
+  expect(getByText('t1')).toBeInTheDocument();
+  expect(getByText('t3')).toBeInTheDocument();
+  // t2 should not be on the screen
+  expect(() => getByText('t2')).toThrow();
+});
+
+test('test delete team', () => {
+  const { getAllByText, getByText } = render(<TeamTable teamsList={teamsList} />)
+  // click on the first delete button (deletes the first team)
+  fireEvent.click(getAllByText('Delete')[0]);
+
+  // the deleted team should no longer be on screen
+  expect(() => getByText('t1')).toThrow();
+})
