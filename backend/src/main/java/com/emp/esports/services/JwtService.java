11b2b2e2bef7948774b183869aba842bcad65311
PotatoOverlord22 <raul.tiron@stud.ubbcluj.ro>
2024-05-28 23:49:44 +0300

    fix security

--- backend/src/main/java/com/emp/esports/services/JwtService.java
+++ backend/src/main/java/com/emp/esports/services/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = "bfa26d6b1a94740d6281af5c9bace394e46c781bb547bdaa7b1e86552d940a5f";
+    @Value("${jwt.secretKey}")
+    private String secretKey;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -38,7 +40,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
